88ee1b6097b4ccdece2b17017bc43327acf7e491
sugi-kun123 <18cookie1212@gmail.com>
2024-03-17 16:00:13 +0900

    first config

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -1,22 +1,29 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'frontend',
+    title: process.env.npm_package_name || '',
+    mode: 'spa',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'ja'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Noto+Sans+JP:400,500,700,900&display=swap' },
+      { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.6.1/css/all.css' },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
 
+  loading: { color: '#fff' },
   // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '~/assets/style/main.scss'
+  ],
   css: [
   ],
 
